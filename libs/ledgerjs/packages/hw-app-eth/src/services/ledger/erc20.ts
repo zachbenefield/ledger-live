acf79c59b4725b86e242ce0304f7cecf85af9c36
Juan Cortés Ross <juan@bohem.io>
2022-07-03 11:12:10 +0200

    feat/LIVE-537 llc logic for bim support

--- libs/ledgerjs/packages/hw-app-eth/src/services/ledger/erc20.ts
+++ libs/ledgerjs/packages/hw-app-eth/src/services/ledger/erc20.ts
@@ -33,7 +33,13 @@ const asContractAddress = (addr: string) => {
   const a = addr.toLowerCase();
   return a.startsWith("0x") ? a : "0x" + a;
 };
-
+00000069                                  // Item data length
+05                                        // Ticker length
+244141504c                                // Ticker data ($AAPL)
+41efc0253ee7ea44400abb5f907fdbfdebc82bec  // Address
+00000012                                  // Decimals
+00000001                                  // Chain id
+3045022100a1e0859e2ad886121b0c5bb374622dcee83b6b0b26a5552559b56a328e4d50ad02202efc09d46a0770a40c6a650a9eec00ba9d8a6727a369398a5f8e3f1d698ccc71
 // this internal get() will lazy load and cache the data from the erc20 data blob
 const get: () => API = (() => {
   let cache;
