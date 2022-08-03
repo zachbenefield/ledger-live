caaafb941b7785ea09ff018a65230ae5fbe6b8b9
Thomas Roger Lux <thomas@rogerlux.dev>
2022-08-03 17:26:51 +0200

    feat(mobile): add completion screen to sync onboarding before redirection

--- apps/ledger-live-mobile/src/const/navigation.ts
+++ apps/ledger-live-mobile/src/const/navigation.ts
@@ -313,6 +313,7 @@ export const ScreenName = {
   OnboardingModalRecoveryPhraseWarning: "OnboardingModalRecoveryPhraseWarning",
 
   SyncOnboardingCompanion: "SyncOnboardingCompanion",
+  SyncOnboardingCompletion: "SyncOnboardingCompletion",
   DeviceModelSelection: "DeviceModelSelection",
   BleDevicesScanning: "BleDevicesScanning",
   BleDevicePairing: "BleDevicePairing",
