d1d0bf70934882afd43038240ee177448872dc60
Emmanuel <emmanuelm41@gmail.com>
2022-11-03 14:48:00 -0300

    Add account methods for casper (#3)
    * feat: add account methods for casper
    * fix: tslib module not found for ts-results
    * feat: add seed identifier path for casper
    * fix: sync for ledger cli
    * feat: add transaction.ts to casper integration
    * fix: change magnitude of casper token to motes (9)
    * chore: generated update
    * fix: make send work for casper
    * feat: implement estimateMaxSpendable for casper
    * remove blake2 package, using pure js version
    
    Co-authored-by: Prateek Rathod <prateek@zondax.ch>

--- libs/ledgerjs/packages/cryptoassets/src/currencies.ts
+++ libs/ledgerjs/packages/cryptoassets/src/currencies.ts
@@ -3650,7 +3650,7 @@ export const cryptocurrenciesById: Record<CryptoCurrencyId, CryptoCurrency> = {
       {
         name: "CSPR",
         code: "CSPR",
-        magnitude: 18,
+        magnitude: 9,
       },
     ],
   },
