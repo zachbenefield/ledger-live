17463364050432731ce04a47df447abac5d11826
Prateek Rathod <prateek@zondax.ch>
2023-06-17 20:24:18 +0530

    refac(cspr): update llc

--- libs/ledger-live-common/src/generated/deviceTransactionConfig.ts
+++ libs/ledger-live-common/src/generated/deviceTransactionConfig.ts
@@ -60,4 +60,4 @@ export type ExtraDeviceTransactionField =
   | ExtraDeviceTransactionField_stellar
   | ExtraDeviceTransactionField_tezos
   | ExtraDeviceTransactionField_tron
-    | ExtraDeviceTransactionField_polkadot;
+  | ExtraDeviceTransactionField_polkadot;
