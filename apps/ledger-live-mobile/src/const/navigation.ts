b3b0f0ba397eb1faf989a3c38e1d9ffdf5cb5b28
Prateek Rathod <prateek@zondax.ch>
2023-08-23 17:22:36 +0530

    refac(cspr): review comments

--- apps/ledger-live-mobile/src/const/navigation.ts
+++ apps/ledger-live-mobile/src/const/navigation.ts
@@ -17,7 +17,6 @@ export enum ScreenName {
   BitcoinEditCustomFees = "BitcoinEditCustomFees",
   ClaimNftWelcome = "ClaimNftWelcome",
   ClaimNftQrScan = "ClaimNftQrScan",
-  CasperEditTransferId = "CasperEditTransferId",
   ClaimRewardsConnectDevice = "ClaimRewardsConnectDevice",
   ClaimRewardsSelectDevice = "ClaimRewardsSelectDevice",
   ClaimRewardsValidationError = "ClaimRewardsValidationError",
@@ -466,6 +465,8 @@ export enum ScreenName {
 
   // stacks
   StacksEditMemo = "StacksEditMemo",
+  // casper
+  CasperEditTransferId = "CasperEditTransferId",
   // hedera
   HederaEditMemo = "HederaEditMemo",
   // near
